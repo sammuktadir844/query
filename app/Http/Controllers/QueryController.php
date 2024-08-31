d1b25d2aa02add3f80de92308de45b7d7e904c55
Sam_844 <sammuktadir844@gmail.com>
2024-08-31 13:14:00 +0600

    first commit

---
+++ app/Http/Controllers/QueryController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Query;
+use App\Http\Requests\StoreQueryRequest;
+use App\Http\Requests\UpdateQueryRequest;
+use Illuminate\Http\Request;
+
+class QueryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        if ($request->ajax()) {
+            $queries =  Query::all();
+            return response()->json(['data' => $queries], 200);
+        }
+
+
+        return view('admin.queries.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreQueryRequest $request)
+    {
+        $query = Query::create([
+            'full_name' => $request->full_name,
+            'email' => $request->email,
+            'Company_name' => $request->Company_name,
+            'phone_number' => $request->phone_number,
+            'message' => $request->message
+        ]);
+
+        return response()->json(['status' => 'success', 'message' => 'Thanks for contacting us!']);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Query $query)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Query $query)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateQueryRequest $request, Query $query)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Query $query)
+    {
+        //
+    }
+}
---
+++ database/migrations/2024_08_31_062116_create_queries_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('queries', function (Blueprint $table) {
+            $table->id();
+            $table->longText('message');
+            $table->text('full_name');
+            $table->string('email');
+            $table->string('Company_name');
+            $table->integer('phone_number');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('queries');
+    }
+};
